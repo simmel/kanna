251db8763abea2e513f9d1d8c734502d26d7c088
Simon Lundström <github-commits@soy.se>
2020-06-21 14:29:38 +0200

    Extend Camel BP main so we can add options

--- src/main/java/se/soy/kanna/Main.java
+++ src/main/java/se/soy/kanna/Main.java
@@ -1,12 +1,17 @@
 package se.soy.kanna;
+import java.util.LinkedList;
 
-public class Main {
+public class Main extends org.apache.camel.blueprint.Main {
   private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(Thread.currentThread().getStackTrace()[1].getClassName());
+
+  public Main() {
+    super();
+  }
 
   public static void main(String[] args) throws Exception {
     log.info("Starting Kanna");
 
-    org.apache.camel.blueprint.Main main = new org.apache.camel.blueprint.Main();
+    Main main = new Main();
     // We must include our own bundle or otherwise supplied blueprint won't be
     // started.
     main.setIncludeSelfAsBundle(true);

7f935dfb452f973861a72adae740a7a62218580d
Simon Lundström <github-commits@soy.se>
2020-06-21 14:14:00 +0200

    Add camel-blueprint-main so we run the OSGi bundle

--- src/main/java/se/soy/kanna/Main.java
+++ src/main/java/se/soy/kanna/Main.java
@@ -3,7 +3,15 @@ package se.soy.kanna;
 public class Main {
   private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(Thread.currentThread().getStackTrace()[1].getClassName());
 
-  public static void main(String[] args) {
-    log.info("Hello world!");
+  public static void main(String[] args) throws Exception {
+    log.info("Starting Kanna");
+
+    org.apache.camel.blueprint.Main main = new org.apache.camel.blueprint.Main();
+    // We must include our own bundle or otherwise supplied blueprint won't be
+    // started.
+    main.setIncludeSelfAsBundle(true);
+
+    // Run until terminated
+    main.run(args);
   }
 }
